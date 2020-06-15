59fa1ff0f538b308b79813e3b32ecc1fed908f8d
wyn <k022298@gmail.com>
2020-06-15 11:53:10 +0800

    Add adaptiveUIDemo

---
+++ AdaptiveUIDemo/AdaptiveUIDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdaptiveUIDemo
+//
+//  Created by wyn on 2020/4/26.
+//  Copyright © 2020 Wyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
