00f384c53e9b5cca02fc6e7fd1dfc9ccc7353151
KaoWenChung <k022298@gmail.com>
2020-12-10 17:02:39 +0800

    修正資料傳輸方式

--- 42-MaskMap/maskMapV3/Controller/MaskVC.swift
+++ 42-MaskMap/maskMapV3/Controller/MaskVC.swift
@@ -10,93 +10,88 @@ import UIKit
 
 class MaskVC: UIViewController {
     @IBOutlet weak var tableView: UITableView!
-    
-    var spinner = UIActivityIndicatorView()
-    
+    private var spinner = UIActivityIndicatorView()
     private var masks = [Mask]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        
-        spinner.style = .medium
+
+        setupSpinner()
+        spinner.startAnimating()
+
+        fetchMaskData()
+    }
+
+    private func setupSpinner() {
+        spinner.style = .large
         spinner.hidesWhenStopped = true
-        view.addSubview(spinner)
-        
+
         // Define the constraint of spinner
         spinner.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([ spinner.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 150.0), spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor)])
-        
-        // Start animation
-        spinner.startAnimating()
-//        getMasks()
-//    }
-//    func getMasks(){
-//        guard let url = URL(string: "https://quality.data.gov.tw/dq_download_json.php?nid=116285&md5_url=2150b333756e64325bdbc4a5fd45fad1")else{ return }
-//        URLSession.shared.dataTask(with: url) { data, response, error in
-//            if let data = data {
-//                if let decodadMasks = try? JSONDecoder().decode([Mask].self, from: data){
-//                    self.masks = decodadMasks
-//                    DispatchQueue.main.async {
-//                        self.tableView.reloadData()
-//                    }
-//                }
-//            }
-//        }.resume()
-//    }
-            // Hit the API endpoint
-            let urlString = "https://quality.data.gov.tw/dq_download_json.php?nid=116285&md5_url=2150b333756e64325bdbc4a5fd45fad1"
+        view.addSubview(spinner)
+        var navHeight: CGFloat = 0.0
+        if let nav = navigationController, nav.navigationBar.frame.maxY > 0 {
+            navHeight = nav.navigationBar.frame.maxY
+        }
 
-            let url = URL(string: urlString)
+        let height = view.frame.height
+        spinner.topAnchor.constraint(equalTo: view.topAnchor, constant: (height / 2) - navHeight).isActive = true
+        spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    private func fetchMaskData() {
+        let urlString = "https://quality.data.gov.tw/dq_download_json.php?nid=116285&md5_url=2150b333756e64325bdbc4a5fd45fad1"
 
-            guard url != nil else { return }
+        guard let url = URL(string: urlString) else { return }
 
-            let session = URLSession.shared
-            let dataTask = session.dataTask(with: url!) { (data, response, error) in
-                if error == nil && data != nil{
-                    // Parse JSON
-                    let decoder = JSONDecoder()
-                    let dateFormatter = DateFormatter()
-                    dateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
-                    decoder.dateDecodingStrategy = .formatted(dateFormatter)
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: url) { (data, response, error) in
+            if error == nil && data != nil{
+                // Parse JSON
+                let decoder = JSONDecoder()
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+                decoder.dateDecodingStrategy = .formatted(dateFormatter)
 
-                    do {
-                    let downloadedMasks = try decoder.decode([Mask].self, from: data!)
-                        self.masks = downloadedMasks
-                        DispatchQueue.main.async {
-                            self.spinner.stopAnimating()
-                            self.tableView.reloadData()
-                        }
-
+                do {
+                let downloadedMasks = try decoder.decode([Mask].self, from: data!)
+                    self.masks = downloadedMasks
+                    DispatchQueue.main.async {
+                        self.spinner.stopAnimating()
+                        self.tableView.reloadData()
                     }
-                    catch{
-                        print("Error in JSON parsing")
-                    }
-
+                }
+                catch{
+                    print("Error in JSON parsing")
                 }
             }
-            dataTask.resume()
         }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showMaskDetail" {
-            if let indexPath = tableView.indexPathForSelectedRow {
-                let destinationController = segue.destination as! HospitalDetailViewController
-                destinationController.masks = masks[indexPath.row]
-            }
-        }
+        dataTask.resume()
     }
 }
-    extension MaskVC: UITableViewDataSource, UITableViewDelegate{
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-                return masks.count
-            }
-            
-            func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-                guard let cell = tableView.dequeueReusableCell(withIdentifier: "MaskCell") as? MaskTableViewCell else { return MaskTableViewCell() }
-                
-                cell.setUpView(mask: masks[indexPath.row])
-                return cell
-            }
+
+extension MaskVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return masks.count
     }
+        
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MaskCell") as? MaskTableViewCell else { return MaskTableViewCell() }
+        
+        cell.setUpView(mask: masks[indexPath.row])
+        return cell
+    }
+}
+
+extension MaskVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let storyboard = storyboard else { return }
+        guard let navigation = navigationController else { return }
+
+        let destionation = storyboard.instantiateViewController(identifier: "HospitalDetailViewController") as! HospitalDetailViewController
+        destionation.maskData = masks[indexPath.row]
+        navigation.pushViewController(destionation, animated: true)
+    }
+}
         
